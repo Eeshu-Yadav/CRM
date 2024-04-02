1f85228d5bc687dcade0646d1b18dd7dbb98e73c
Eeshu-Yadav <eeshu2212081@akgec.ac.in>
2024-04-02 15:11:48 +0530

    c-ommit

--- c_r_m/c_r_m/urls.py
+++ c_r_m/c_r_m/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+
+
+    path('', include('web_app.urls')),
 ]
